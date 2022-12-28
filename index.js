63842258f3f9b4bae3b61654027d1fb77451255c
Adii <115168602+yng-blood@users.noreply.github.com>
2022-12-28 08:15:02 +0530

    dom

---
+++ index.js
@@ -0,0 +1,11 @@
+let btnimp = document.getElementById("allchange");
+let flexBox = document.getElementById("newboxchange")
+btnimp.addEventListener("click", function() {
+    flexBox.style.flexDirection = "column";
+});
+
+let btnimp = document.getElementById("allchange");
+let flexBox = document.getElementById("newboxchange")
+btnimp.addEventListener("click", function() {
+    flexBox.style.flexDirection = "column";
+});
